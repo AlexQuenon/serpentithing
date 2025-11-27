27d1b4131aa0a8682f00aa93ba9c0072540dfcac
Alex Quenon <Alex.Quenon@gmail.com>
2025-11-27 00:04:04 -0500

    countdown

---
+++ code/hidden.gd
@@ -0,0 +1,9 @@
+extends Label
+
+
+func _on_snake_coordinates_updated(snake_coords, block_coords, last_direction):
+	if snake_coords.size() + block_coords.size() == 10:
+		hide()
+	else:
+		show()
+		text = "+%s" % (10 - snake_coords.size())
