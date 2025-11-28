56ef6367bbae43780319f583e8e822d5b3baf79e
Alex Quenon <Alex.Quenon@gmail.com>
2025-11-27 21:03:38 -0500

    fix fullscreen

--- code/fullscreen.gd
+++ code/fullscreen.gd
@@ -1,16 +1,8 @@
-extends CheckButton
+extends Button
 
 
-func _on_toggled(toggled_on):
-	if toggled_on:
-		DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN)
-	else:
+func _on_pressed():
+	if get_window().mode == DisplayServer.WINDOW_MODE_FULLSCREEN:
 		DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_WINDOWED)
-
-
-func _process(_delta):
-	# Handle cases where the window mode has changed outside of pressing the button
-	if get_window().mode == DisplayServer.WINDOW_MODE_FULLSCREEN and not button_pressed:
-		button_pressed = true
-	elif button_pressed:
-		button_pressed = false
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN)
